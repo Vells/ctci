12f6a88d95babbc3971c35ee50b93d746203f3ef
Vells <vdmineva@gmail.com>
2016-08-05 10:10:19 -0700

    Probelm 1.3: Added another solution

--- chapter1/1.3_permutations.py
+++ chapter1/1.3_permutations.py
@@ -15,5 +15,30 @@ def are_permutations(str1, str2):
     return sorted(str1) == sorted(str2)
 
 
+def are_permutations_1(word, other_word):
+    """
+    Time complexity: 0(n)
+    Space complexity: 0(1)
+    """
+    if len(word) != len(other_word):
+        return False
+
+    # Assumption: always check with the interviewer the size of the character set!!!
+    letters = [0] * 256
+
+    for char in word:
+        letters[ord(char)] += 1
+
+    for char in other_word:
+        letters[ord(char)] -= 1
+        if letters[ord(char)] < 0:
+            return False
+
+    return True
+
+
 print are_permutations("aba", "aab") is True
 print are_permutations("asd", "gfd") is False
+
+print are_permutations_1("vamb", "Badm") is False
+print are_permutations_1("abba", "baba") is True
